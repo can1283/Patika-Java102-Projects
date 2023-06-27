75dfd18f147ea6db7b5275b78b8b36340855923c
Nurican Kaşıkcı <112473835+can1283@users.noreply.github.com>
2023-06-27 11:40:33 +0300

    Create Book.java

---
+++ Book Comparator/Book.java
@@ -0,0 +1,44 @@
+public class Book implements Comparable<Book> {
+    private String bookName;
+    private int pageCount;
+    private String authorName;
+    private Date publicationDate;
+
+    public Book(String bookName, int pageCount, String authorName, Date publicationDate) {
+        this.bookName = bookName;
+        this.pageCount = pageCount;
+        this.authorName = authorName;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    @Override
+    public int compareTo(Book otherBook) {
+        return this.bookName.compareTo(otherBook.bookName);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", pageCount=" + pageCount +
+                ", authorName='" + authorName + '\'' +
+                ", publicationDate=" + publicationDate +
+                '}';
+    }
+}
